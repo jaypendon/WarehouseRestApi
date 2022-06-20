36e83aed94634a852fa97513c0b043c7cac97a06
Jay Pendon <jay@jpendon.com>
2022-06-20 16:26:47 -0400

    Initial Commit

---
+++ src/main/java/com/jpendon/WarehouseRestAPI/WarehouseRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.jpendon.WarehouseRestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpendon/WarehouseRestAPI/WarehouseRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpendon.WarehouseRestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
