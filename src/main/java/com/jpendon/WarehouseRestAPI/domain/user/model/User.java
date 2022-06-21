93daa582b08b406e04e02fec32e17d42834a0df4
Jay Pendon <jay@jpendon.com>
2022-06-20 20:49:57 -0400

    Removed user/password

--- src/main/java/com/jpendon/WarehouseRestAPI/domain/user/model/User.java
+++ src/main/java/com/jpendon/WarehouseRestAPI/domain/user/model/User.java
@@ -16,12 +16,6 @@ public class User {
 	@Column(name="id")
 	private Long id; 
 	
-	@Column(name="username")
-	private String username; 
-	
-	@Column(name="password")
-	private String password;
-	
 	@Column(name="first_name")
 	private String firstName; 
 	
@@ -52,22 +46,6 @@ public class User {
 
 	public void setId(Long id) {
 		this.id = id;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
 	}
 
 	public String getFirstName() {
